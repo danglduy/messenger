f88a6bfc8ddc24b4746cf5f692e515e38fb982c6
danglduy <38782010+danglduy@users.noreply.github.com>
2022-10-30 22:47:55 +0700

    Add react-router

--- app/javascript/home/App.tsx
+++ app/javascript/home/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { createHashRouter, RouterProvider, Route } from 'react-router-dom';
 import Home from './components/Home';
 
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+]);
+
 const App = () => {
-  return <Home />;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
