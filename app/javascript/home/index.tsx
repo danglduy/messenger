457f824872bfa4d9f1d8eda7d54d793a5e07004c
danglduy <38782010+danglduy@users.noreply.github.com>
2022-10-30 18:19:50 +0700

    Add React

---
+++ app/javascript/home/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const App = () => {
+  return <div>aaaaaaaaaaaaaaaaaaaaa</div>;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("root");
+  if (container) {
+    const root = createRoot(container);
+    root.render(<App />);
+  }
+});
