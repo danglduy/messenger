87f29f2bd6f9b1d9cfa1977e186e770b4445c999
danglduy <38782010+danglduy@users.noreply.github.com>
2022-10-30 21:43:58 +0700

    Extract sidebar components

--- app/javascript/home/App.tsx
+++ app/javascript/home/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Layout from './components/Layout';
+import Home from './components/Home';
 
 const App = () => {
-  return <Layout />;
+  return <Home />;
 };
 
 export default App;
