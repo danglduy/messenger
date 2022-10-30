3932f533e7e1ba9afe49bfc1304a9f902d23c3a5
danglduy <38782010+danglduy@users.noreply.github.com>
2022-10-30 23:13:20 +0700

    Update db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,13 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.where(email: "user@domain.com")
-    .first_or_create(password: "12345678")
+user1 = User.where(email: "user@domain.com")
+            .first_or_create(password: "12345678")
+
+user2 = User.where(email: "user2@domain.com")
+            .first_or_create(password: "12345678")
+
+channel1 = Channel.where(name: "channel_1").first_or_create
+channel1.users = [user1, user2]
+channel1.save!
+
