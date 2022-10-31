1748c65850969c7f224d0b3ccd18da096e9a60fa
danglduy <38782010+danglduy@users.noreply.github.com>
2022-10-31 11:13:28 +0700

    Add Messages component

--- app/javascript/home/App.tsx
+++ app/javascript/home/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { createHashRouter, RouterProvider, Route } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Home from './components/Home';
+import Messages from './components/Messages';
 
 const router = createHashRouter([
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/channels/:id',
+    element: <Messages />,
   },
 ]);
 
---
+++ app/javascript/home/components/Messages/Messages.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Layout from '../Layout';
+
+export function Messages() {
+  return (
+    <Layout>
+      <main className="flex-1">
+        <div className="py-6">
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
+            <h1 className="text-2xl font-semibold text-gray-900">Messages</h1>
+          </div>
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
+            {/* Replace with your content */}
+            <div className="py-4">
+              <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
+            </div>
+            {/* /End replace */}
+          </div>
+        </div>
+      </main>
+    </Layout>
+  );
+}
