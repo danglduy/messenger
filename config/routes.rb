030561ca4791420aeb2adaa76db81b3469a77772
danglduy <38782010+danglduy@users.noreply.github.com>
2022-10-31 10:27:55 +0700

    Channels api list

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   delete :logout, to: 'sessions#destroy'
 
   namespace :api do
+    resources :channels, only: [:index]
     resources :users, only: [:index]
   end
 end
