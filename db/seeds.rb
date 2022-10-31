a29752ae1cb541c75be98e813e4e1924045c8c5f
danglduy <38782010+danglduy@users.noreply.github.com>
2022-10-31 18:22:24 +0700

    Update db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,25 @@ user1 = User.where(email: "user@domain.com")
 user2 = User.where(email: "user2@domain.com")
             .first_or_create(name: "User 2", password: "12345678")
 
+user3 = User.where(email: "user3@domain.com")
+            .first_or_create(name: "User 3", password: "12345678")
+
 channel1 = Channel.where(name: "Channel 1").first_or_create
 channel1.users = [user1, user2]
 channel1.save!
+
+channel2 = Channel.where(name: "Channel 2").first_or_create
+channel2.users = [user1, user2, user3]
+channel2.save!
 
 Message.where(channel: channel1, user: user1)
                  .first_or_create(content: "First message")
 Message.where(channel: channel1, user: user2)
                  .first_or_create(content: "Second message")
+
+Message.where(channel: channel2, user: user1)
+                 .first_or_create(content: "First message 2")
+Message.where(channel: channel2, user: user2)
+                 .first_or_create(content: "Second message 2")
+Message.where(channel: channel2, user: user3)
+                 .first_or_create(content: "Third message 2")
